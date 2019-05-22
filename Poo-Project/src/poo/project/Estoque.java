b9df1ab3e2c310b2344ef4a82b0612c43ba7ccd6
joaov <joaov@DESKTOP-GOL7OFU>
2019-05-22 20:22:03 -0300

    teste juan viado

--- Poo-Project/src/poo/project/Estoque.java
+++ Poo-Project/src/poo/project/Estoque.java
@@ -1,15 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package poo.project;
 
 
-/**
- *
- * @author isagu
- */
+//teste
 public class Estoque {
     
     byte tipo;
