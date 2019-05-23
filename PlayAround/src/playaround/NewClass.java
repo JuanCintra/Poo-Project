b139d8c403f101afabc58e975b914d9d56dcb8f5
JuanCintra <isagu@DESKTOP-A9RTCON>
2019-05-23 18:14:04 -0300

    Venda pode ser buscada, e o estoque diminui com cada venda!

---
+++ PlayAround/src/playaround/NewClass.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package playaround;
+
+/**
+ *
+ * @author isagu
+ */
+public class NewClass {
+    void media(){
+        int x[]={1,2};
+        for(int y=0;y<2;y++){
+            System.out.println(x[y]);
+        }
+    }
+}
